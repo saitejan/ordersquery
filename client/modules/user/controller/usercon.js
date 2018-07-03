f0e29b69e2a14784820e728e41fa8c5de8f4fc8f
saitejan <39676087+saitejan@users.noreply.github.com>
2018-07-03 10:29:05 +0530

    Delete usercon.js

--- client/modules/user/controller/usercon.js
+++
@@ -1,8 +0,0 @@
-angular.module('appUserCtrl',[])
-    .controller('UserCtrl',function ($scope,$http,$state,UserService) {
-    $scope.oneuserData	= UserService.getUserData();
-    	console.log('userData : '+JSON.stringify($scope.oneuserData))
-    	
-    
-
-})
